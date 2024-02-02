f822290857a040755682b52423ed6efa44de3b23
nazwaputritania <nputritania@gmail.com>
2024-02-02 09:45:22 +0700

    Tugas template Nazwa Putri Tania XI RPL 2

---
+++ resources/views/coba.blade.php
@@ -0,0 +1,9 @@
+@extends('template.master')
+
+@section('header', 'coba')
+
+@section('content')
+<button class="btn btn-primary">Halo semua</button>
+
+@endsection
+    
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/template', function () {
+    return view('template.master');
 });
+
+Route::get('/coba', function () {
+    return view('coba');
+});
+
+
